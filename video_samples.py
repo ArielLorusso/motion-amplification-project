d09c3bd752fd71ffddf76cd1a830df7fdac37a4e
Ariel Lorusso <ariellorusso@gmail.com>
2026-01-23 14:09:27 -0300

    reimplemented rectangle daw funcion

--- video_samples.py
+++ video_samples.py
@@ -22,8 +22,6 @@ opencv-python            4.12.0.88
 numpy                    1.26.4
 '''
 
-math.sin
-
 # time Parameters
 fps = 60
 seconds = 10
@@ -47,15 +45,23 @@ fourcc = cv.VideoWriter_fourcc(*"mp4v")  #  other options : DIVX, XVID, MJPG, X2
 out   = cv.VideoWriter("circle-sqare.mp4", fourcc, fps, (video_W, video_H))
 # out   = video generated 
 
-# def centered_rectangle (img, center_x,center_y,width,height, color) :
-#    p1 = (center_x - width//2, center_y - height//2)
-#    p2 = (center_x + width//2, center_y + height//2)
-#    img[skimage.draw.rectangle(circle_mov[i], radius=radius), shape=img.shape] = color1
-#    cv.rectangle(img, p1, p2, color, thickness=-1 )        #//  OLD   no subpixel
-#    return img
+"""
+def rotation_movement( f_start, f_end, frames):
 
 
-def centered_rectangle(img, pos, width, height, color):
+    # Define the rotation angle
+    angle = 45  # Rotate by 45 degrees
+    # Define the scaling factor
+    scale = 1.0  # No scaling
+    # Get the rotation matrix
+    rotation_matrix = cv.getRotationMatrix2D(center, angle, scale) 
+    # Perform the rotation
+    rotated_image = cv.warpAffine(image, rotation_matrix, (width, height))
+    # https://opencv.org/blog/image-rotation-and-translation-using-opencv/#:~:text=1-,cv2.getRotationMatrix2D
+
+"""
+def rectangle(img, pos, sca , color):
+    width, height = sca
     row, col = pos  # floats
     
     hh = height / 2
@@ -77,21 +83,21 @@ def centered_rectangle(img, pos, width, height, color):
     
     cv.fillPoly(img, [pts], color, lineType=cv.LINE_AA, shift=shift)  
 
-def subpixel_ellipse(img, pos, axes, color):
+def ellipse(img, pos, sca, color):
     """
-    center: (y0, x0) floats
-    axes: (a, b) floats (semi-major and semi-minor axes)
+    pos: (y, x) position  (float)
+    sca: (a, b) float
     """
-    y0, x0 = pos
-    a, b = axes # a is vertical radius, b is horizontal radius
+    y, x = pos
+    a, b = sca # a is vertical radius, b is horizontal radius
     
     # 1. Define bounding box (add padding for the AA ramp)
     pad = max(a, b) + 2
-    top, bottom = int(y0 - pad), int(y0 + pad)
-    left, right = int(x0 - pad), int(x0 + pad)
+    top, bottom = int(y - pad), int(y + pad)
+    left, right = int(x - pad), int(x + pad)
     
     # Clip to image boundaries
-    top, bottom = max(0, top), min(img.shape[0], bottom)
+    top, bottom = max(0, top ), min(img.shape[0], bottom)
     left, right = max(0, left), min(img.shape[1], right)
     
     # 2. Create grid
@@ -99,7 +105,7 @@ def subpixel_ellipse(img, pos, axes, color):
     
     # 3. Calculate Normalized Distance (d^2)
     # Inside the ellipse, value < 1. Outside, value > 1.
-    dist_sq = ((yy - y0)**2 / a**2) + ((xx - x0)**2 / b**2)
+    dist_sq = ((yy - y)**2 / a**2) + ((xx - x)**2 / b**2)
     dist = np.sqrt(dist_sq)
     
     # 4. Anti-aliasing Mask
@@ -114,10 +120,8 @@ def subpixel_ellipse(img, pos, axes, color):
     img[top:bottom, left:right] = (1 - mask) * img_crop + mask * color
 
 
-
 def initial_position(frames,x,y):
     pos = np.tile([x,y], (frames,1))
-
     return pos
 
 def linear_movement (pos,x,y,x2,y2,f_start,f_end,frames):
@@ -136,34 +140,36 @@ def linear_movement (pos,x,y,x2,y2,f_start,f_end,frames):
 
 def sinusoidal_movement(pos, x_amp, y_amp, x_freq, y_freq, f_start, f_end, frames, x_phase=0, y_phase=0):
     """
-    Generate sinusoidal oscillation in x-axis
-    
+    Generates sinusoidal x and y position oscillation.
+    position is a numpy array  [ [x,y],[x,y]... ] 
+    ( y axis arguments have same behaviour as x ones )
+    this function adds to the original position for easy composed motion
+
     arguments:
         pos      : [ [x,y],[x,y]... ]  position array to modify
-        x_center : center x position around which to oscillate
         x_amp    : amplitude of x axis oscillation in pixels
         x_freq   : number of complete cycles during the movement period
         f_start  : start frame
         f_end    : end frame
         frames   : total frames (for holding position after f_end)
         x_phase  : initial phase offset in radians (0 to 360)
-    """
-
+   """
+    # phace is converted to degree  (if deleted radiasn are used instead)
     x_phase = x_phase / 180 * math.pi  
     y_phase = y_phase / 180 * math.pi  
     
     for i in range(f_start, f_end):
-        # Normalized time from 0 to 1 over the duration
+        # calculate fime in seconds given a frame i
         t = (i - f_start) / fps
         
         # Calculate sinusoidal position
-        # 2π * frequency * t gives the angle for the desired number of cycles
+        # 2π * frequency * t gives the angle,  used for oscilation
         x_angle = 2 * math.pi * x_freq * t + x_phase
         x =  x_amp * math.sin(x_angle)
-        
+        # same for y axis
         y_angle = 2 * math.pi * y_freq * t + y_phase
         y = y_amp * math.sin(y_angle)
-        
+        # update position vector
         pos[i, 0] += y     # row
         pos[i, 1] += x     # column
     
@@ -176,6 +182,43 @@ def sinusoidal_movement(pos, x_amp, y_amp, x_freq, y_freq, f_start, f_end, frame
     
     return pos
 
+def sinusoidal_rotation(rot, amp, freq, phase, f_start, f_end, frames):
+
+    """
+    Generate sinusoidal oscillation in x-axis
+    
+    arguments:
+        pos      : [ [x,y],[x,y]... ]  position array to modify
+
+        x_amp    : amplitude of x axis oscillation in pixels
+        x_freq   : number of complete cycles during the movement period
+        f_start  : start frame
+        f_end    : end frame
+        frames   : total frames (for holding position after f_end)
+        x_phase  : initial phase offset in radians (0 to 360)
+    """
+    if pos == None :
+        pos = np.tile([phase], (frames,1))
+
+    for i in range(f_start, f_end):
+        # calculate fime given the frame
+        t = (i - f_start) / fps
+        
+        # Calculate sinusoidal position
+        # 2π * frequency * t gives the angle for the desired number of cycles
+        phase = 2 * math.pi * freq * t 
+        angle =  amp * math.sin(angle)
+
+        pos[i, 1] += x     # column
+    
+    # Hold the last position for remaining frames
+    if f_end < frames:
+        last_x, last_y = pos[f_end - 1]
+        for i in range(f_end, frames):
+            pos[i, 0] = last_y
+            pos[i, 1] = last_x
+    
+    return pos
 
 y = video_H / 2
 x = video_W / 2
@@ -193,7 +236,7 @@ def linear_animation ():
 rectang_mov,circle_mov = linear_animation()
 
 
-def sinusoidal_animation ():
+def sine_position_animation ():
     rectang_mov = initial_position(frames, y, x)
     # rectang_mov = sinusoidal_movement(rectang_mov,x_amp=350.5,  x_freq=.80,  x_phase=0, 
     #                                               y_amp=200.5,  y_freq=.30 , y_phase=90,    
@@ -205,25 +248,26 @@ def sinusoidal_animation ():
     #return rectang_mov,circle_mov
     return rectang_mov
 
-rectang_mov = sinusoidal_animation()
+#######################*  DRAWING LOOP  *############################################################333
+
+rectang_pos = sine_position_animation ()     # same position and scale share dimention [x,y][i]
+rectang_sca = sine_position_animation () /10 # so thers no need for making new functions, 
 
 for i in range(frames):     # i = frame number : from 0 to frames -1
     img = np.zeros((video_H,video_W, 3), dtype=np.uint8)  # Refresh image to (RGB) black  
     # By moving previous line otside the for loop we get frame acumulation (usefull to debug)
 
-    centered_rectangle(img, rectang_mov[i] , width=6, height=6, color=color2)
-    subpixel_ellipse  (img, circle_mov[i], (radius*2,  radius/2), color=color1)
+#    centered_rectangle(img, rectang_pos[i] , width=6, height=6, color=color2)
+    rectangle(img, rectang_pos[i] , rectang_sca[i], color=color2)  
+    ellipse  (img, circle_mov[i], (radius*2,  radius/2), color=color1)
     out.write(img)
 
 out.release()
 
+# This script is good for some test but improvements can be done to it
 
-
-# This code is a proof of concept
-# it just makes a circle and square to move in oposite horizontal directions
-
-#todo   implement sinusoidal motion    DONE
 #todo   implement scale /ratation motion
 #todo   implement color hue rotation (time)
-#todo   implement color gradients  (space)
+#todo   implement color gradients    (space)
+
 
