c716cbf7204168789dd84c26f777b5f89c9f9ea7
Ariel Lorusso <ariellorusso@gmail.com>
2026-01-23 15:59:25 -0300

    implemented rotation

--- video_samples.py
+++ video_samples.py
@@ -1,13 +1,7 @@
-#  Tue 20 Jan 2026 23:01:38  Start
-#  Tue 21 Jan 2026 12:01:38  1st commit
-#  
+
 #  The purpose of this script is generating simple video samples
 #  This will be used to test the motion_amplification.py script 
 #  in a clear and consistent maner
-#
-#
-#
-#
 
 
 import cv2 as cv     # Import OpenCv library
@@ -40,85 +34,93 @@ color2 = (255,   0,   0) # RED
 
 #* create video object named out
 fourcc = cv.VideoWriter_fourcc(*"mp4v")  #  other options : DIVX, XVID, MJPG, X264, WMV1, WMV2
+out   = cv.VideoWriter("circle-sqare.mp4", fourcc, fps, (video_W, video_H))
 # fourcc  = video codec  https://fourcc.org/codecs.php
 # https://docs.opencv.org/4.x/dd/d9e/classcv_1_1VideoWriter.html
-out   = cv.VideoWriter("circle-sqare.mp4", fourcc, fps, (video_W, video_H))
 # out   = video generated 
 
-"""
-def rotation_movement( f_start, f_end, frames):
 
-
-    # Define the rotation angle
-    angle = 45  # Rotate by 45 degrees
-    # Define the scaling factor
-    scale = 1.0  # No scaling
-    # Get the rotation matrix
-    rotation_matrix = cv.getRotationMatrix2D(center, angle, scale) 
-    # Perform the rotation
-    rotated_image = cv.warpAffine(image, rotation_matrix, (width, height))
-    # https://opencv.org/blog/image-rotation-and-translation-using-opencv/#:~:text=1-,cv2.getRotationMatrix2D
-
-"""
-def rectangle(img, pos, sca , color):
+def rectangle(img, pos, sca, rot, color):
+    """
+    Draw rotated rectangle with subpixel precision
+    
+    Arguments:
+        img: Image array
+        pos: (row, col) position as floats
+        sca: (width, height) tuple
+        rot: rotation angle in degrees
+        color: BGR color tuple
+    """
     width, height = sca
-    row, col = pos  # floats
+    row, col = pos
     
     hh = height / 2
     hw = width / 2
     
-    # OpenCV uses (x, y) format, not (row, col)
-    # and needs integer coordinates with fractional shift
-    shift = 4  # 2^4 = 16 subpixel precision
-    scale = 2 ** shift
-    
+    # Create rectangle corners centered at origin
     pts = np.array([
-        [col - hw, row - hh],
-        [col + hw, row - hh],
-        [col + hw, row + hh],
-        [col - hw, row + hh]
-    ]) * scale
+        [-hw, -hh],  # top-left
+        [+hw, -hh],  # top-right
+        [+hw, +hh],  # bottom-right
+        [-hw, +hh]   # bottom-left
+    ], dtype=np.float32)
     
-    pts = pts.astype(np.int32)
+    # Apply rotation if needed
+    if rot != 0:
+        angle_rad = rot * math.pi / 180
+        cos_a = math.cos(angle_rad)
+        sin_a = math.sin(angle_rad)
+        
+        # Rotation matrix
+        rotation_matrix = np.array([
+            [cos_a, -sin_a],
+            [sin_a,  cos_a]
+        ])
+        
+        # Rotate all points
+        pts = pts @ rotation_matrix.T
     
-    cv.fillPoly(img, [pts], color, lineType=cv.LINE_AA, shift=shift)  
+    # Translate to final position (OpenCV uses x, y which is col, row)
+    pts[:, 0] += col  # x-coordinate (column)
+    pts[:, 1] += row  # y-coordinate (row)
+    
+    # Subpixel rendering
+    shift = 4
+    scale = 1 << shift  # 2^4 = 16
+    pts_scaled = (pts * scale).astype(np.int32)
+    
+    cv.fillConvexPoly(img, pts_scaled, color, lineType=cv.LINE_AA, shift=shift)
+    # ALTERNATIVE 
+    # img = cv.warpAffine(img, rotation_matrix, (width, height))
+    # https://opencv.org/blog/image-rotation-and-translation-using-opencv/#:~:text=1-,cv2.getRotationMatrix2D
 
-def ellipse(img, pos, sca, color):
-    """
-    pos: (y, x) position  (float)
-    sca: (a, b) float
-    """
-    y, x = pos
-    a, b = sca # a is vertical radius, b is horizontal radius
-    
-    # 1. Define bounding box (add padding for the AA ramp)
-    pad = max(a, b) + 2
-    top, bottom = int(y - pad), int(y + pad)
-    left, right = int(x - pad), int(x + pad)
-    
-    # Clip to image boundaries
-    top, bottom = max(0, top ), min(img.shape[0], bottom)
-    left, right = max(0, left), min(img.shape[1], right)
-    
-    # 2. Create grid
-    yy, xx = np.ogrid[top:bottom, left:right]
-    
-    # 3. Calculate Normalized Distance (d^2)
-    # Inside the ellipse, value < 1. Outside, value > 1.
-    dist_sq = ((yy - y)**2 / a**2) + ((xx - x)**2 / b**2)
-    dist = np.sqrt(dist_sq)
-    
-    # 4. Anti-aliasing Mask
-    # We create a smooth transition at the boundary (dist = 1.0)
-    # The 'width' of the smoothing is roughly 1/min(a,b) pixels
-    smoothing = 1.0 / min(a, b)
-    mask = np.clip((1.0 - dist) / smoothing + 0.5, 0, 1)
-    
-    # 5. Blend
-    mask = mask[:, :, np.newaxis]
-    img_crop = img[top:bottom, left:right]
-    img[top:bottom, left:right] = (1 - mask) * img_crop + mask * color
 
+def ellipse(img, pos, sca, rot, color):
+    """
+    Draw rotated ellipse with subpixel precision
+    
+    Arguments:
+        img: Image array
+        pos: (row, col) center position
+        sca: (width, height) - full diameter, not radius
+        rot: rotation angle in degrees
+        color: BGR color tuple
+    """
+    row, col = pos
+    width, height = sca
+    
+    # OpenCV ellipse uses subpixel coordinates with shift
+    shift = 4
+    scale = 1 << shift
+    
+    center = (int(col * scale), int(row * scale))
+    axes   = (int(width / 2 * scale), int(height / 2 * scale))  # Semi-axes
+    
+    cv.ellipse(img, center, axes,
+               angle=rot,
+               startAngle=0, endAngle=360,
+               color=color, thickness=-1,
+               lineType=cv.LINE_AA, shift=shift)
 
 def initial_position(frames,x,y):
     pos = np.tile([x,y], (frames,1))
@@ -136,7 +138,6 @@ def linear_movement (pos,x,y,x2,y2,f_start,f_end,frames):
         pos [i,0] = y  
         pos [i,1] = x 
     return pos
-
 
 def sinusoidal_movement(pos, x_amp, y_amp, x_freq, y_freq, f_start, f_end, frames, x_phase=0, y_phase=0):
     """
@@ -185,20 +186,20 @@ def sinusoidal_movement(pos, x_amp, y_amp, x_freq, y_freq, f_start, f_end, frame
 def sinusoidal_rotation(rot, amp, freq, phase, f_start, f_end, frames):
 
     """
-    Generate sinusoidal oscillation in x-axis
+    Generate sinusoidal oscillation 
     
     arguments:
         pos      : [ [x,y],[x,y]... ]  position array to modify
 
-        x_amp    : amplitude of x axis oscillation in pixels
+        x_amp    : amplitude of angular change
         x_freq   : number of complete cycles during the movement period
         f_start  : start frame
         f_end    : end frame
         frames   : total frames (for holding position after f_end)
         x_phase  : initial phase offset in radians (0 to 360)
     """
-    if pos == None :
-        pos = np.tile([phase], (frames,1))
+    if rot == None :
+        rot = np.full(frames, phase, dtype=np.float32)
 
     for i in range(f_start, f_end):
         # calculate fime given the frame
@@ -206,68 +207,85 @@ def sinusoidal_rotation(rot, amp, freq, phase, f_start, f_end, frames):
         
         # Calculate sinusoidal position
         # 2π * frequency * t gives the angle for the desired number of cycles
-        phase = 2 * math.pi * freq * t 
-        angle =  amp * math.sin(angle)
+        phase = 2 * math.pi * freq * t      # variable phase is repurpused
+        angle_offset =  amp * math.sin(phase)
 
-        pos[i, 1] += x     # column
+        rot[i] = phase + angle_offset  # Set angle (center + oscillation)
     
-    # Hold the last position for remaining frames
+    # Hold the last rotation for remaining frames
     if f_end < frames:
-        last_x, last_y = pos[f_end - 1]
+        last = rot[f_end - 1]
         for i in range(f_end, frames):
-            pos[i, 0] = last_y
-            pos[i, 1] = last_x
+            rot[i] = last
+
     
-    return pos
+    return rot
 
 y = video_H / 2
 x = video_W / 2
 
+#######################*  ANIMATION FUNCTIONS  *############################################################333
+
+# ! Reimplement as :     shape_ani = (shape_pos, shape_sca, shape_rot)
+
+
 def linear_animation ():
-    rectang_mov = initial_position(frames,y,100.0)
-    rectang_mov = linear_movement( rectang_mov, 1.0, y, 3.0,y,  0,60,frames)
-    rectang_mov = linear_movement( rectang_mov, 3.0, y, 1.0,y,  60,frames,frames)
+    rectang_pos = initial_position(frames,y,100.0)
+ #   rectang_pos = linear_movement( rectang_pos, 1.0, y, 3.0,y,  0,60,frames)
+ #   rectang_pos = linear_movement( rectang_pos, 3.0, y, 1.0,y,  60,frames,frames)
 
-    circle_mov  =  initial_position(frames,y,100.0)
-    circle_mov  = linear_movement( circle_mov, 3.0,y, 1.0, y,   0,60,frames)
-    circle_mov  = linear_movement( circle_mov, 1.0, y, 3.0,y,   60,frames,frames)
-    return rectang_mov,circle_mov 
+    circle_pos  = initial_position(frames,y,300.0)
+ #   circle_pos  = linear_movement( circle_pos, 3.0,y, 1.0, y,   0,60,frames)
+ #   circle_pos  = linear_movement( circle_pos, 1.0, y, 3.0,y,   60,frames,frames)
 
-rectang_mov,circle_mov = linear_animation()
+    
+    return rectang_pos,circle_pos 
 
+rectang_pos, circle_pos = linear_animation()
 
+'''
 def sine_position_animation ():
     rectang_mov = initial_position(frames, y, x)
     # rectang_mov = sinusoidal_movement(rectang_mov,x_amp=350.5,  x_freq=.80,  x_phase=0, 
     #                                               y_amp=200.5,  y_freq=.30 , y_phase=90,    
     #                                               f_start=0, f_end=frames, frames=frames)   #* Moire paretn test
-    rectang_mov = sinusoidal_movement(rectang_mov,x_amp=2.5,  x_freq=16,  x_phase=0, 
+    rectang_pos = sinusoidal_movement(rectang_mov,x_amp=2.5,  x_freq=16,  x_phase=0, 
                                                   y_amp=4.5,  y_freq=8 ,  y_phase=90,    f_start=0, f_end=frames, frames=frames)
-    rectang_mov = sinusoidal_movement(rectang_mov,x_amp=100.5, x_freq=0.5, x_phase=0, 
+    rectang_pos = sinusoidal_movement(rectang_mov,x_amp=100.5, x_freq=0.5, x_phase=0, 
                                                   y_amp=100.5,  y_freq=0.5, y_phase=110 , f_start=0, f_end=frames, frames=frames)
     #return rectang_mov,circle_mov
-    return rectang_mov
+    return rectang_pos
+'''
+
+angle = sinusoidal_rotation(None, amp=45, freq=2, phase=45, 
+                           f_start=120, f_end=240, frames=frames)  # from 0 to 90    (45-45)  to (45+45)
+
+# rectang_pos = sine_position_animation ()     # same position and scale share dimention [x,y][i]
+# rectang_sca = sine_position_animation () /10 # so thers no need for making new functions, 
 
 #######################*  DRAWING LOOP  *############################################################333
-
-rectang_pos = sine_position_animation ()     # same position and scale share dimention [x,y][i]
-rectang_sca = sine_position_animation () /10 # so thers no need for making new functions, 
 
 for i in range(frames):     # i = frame number : from 0 to frames -1
     img = np.zeros((video_H,video_W, 3), dtype=np.uint8)  # Refresh image to (RGB) black  
     # By moving previous line otside the for loop we get frame acumulation (usefull to debug)
 
 #    centered_rectangle(img, rectang_pos[i] , width=6, height=6, color=color2)
-    rectangle(img, rectang_pos[i] , rectang_sca[i], color=color2)  
-    ellipse  (img, circle_mov[i], (radius*2,  radius/2), color=color1)
+    rectangle(img, rectang_pos[i] , (180,90) ,angle[i],   color=color2)  
+    ellipse  (img, circle_pos[i]  , (90,180),angle[i],   color=color1)
     out.write(img)
 
 out.release()
 
 # This script is good for some test but improvements can be done to it
 
-#todo   implement scale /ratation motion
-#todo   implement color hue rotation (time)
-#todo   implement color gradients    (space)
+#todo   Add :     scale /ratation motion
+#todo   Add :     color hue rotation (time)
+#todo   Add :     color gradients    (space)
 
-
+#todo   Add : timing profilet
+#todo   Reimplement position with cv2.warpAffine  and compare performance
+        # translation_matrix = np.float32([
+        #     [1, 0, tx],
+        #     [0, 1, ty]
+        # ])
+        # translated_image = cv2.warpAffine(rotated_image, translation_matrix, (width, height))
